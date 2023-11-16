8b8d27a9b15c52213cad3825ac62b46fcbb6d583
FelipeEliasSoares <soaresfelipe396@gmail.com>
2023-11-15 22:08:26 -0300

    Aula 14

--- AULA 14/Praticando03.js
+++ AULA 14/Praticando03.js
@@ -2,11 +2,13 @@ $(document).ready(function () {
     $('img').on({
         mouseover: function () {
             $('body').css('background-color', 'black');
-            $('h2').css('color', 'white');
+            $('h2').css('color', 'white')
+            $('#h2').css('color', 'white').html('Lâmpada Apagada');
         },
         mouseout: function () {
             $('body').css('background-color', 'white');
-            $('h2').css('color', 'black');
+            $('h2').css('color', 'black')
+            $('#h2').css('color', 'black').html('Lâmpada Acesa');
         },
 
     })
